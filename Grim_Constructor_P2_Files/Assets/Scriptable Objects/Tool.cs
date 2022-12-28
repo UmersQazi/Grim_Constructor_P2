8df7ecec240bc06fbebe9f008b88c5615d6f864e
UmersQazi <umersqazi@gmail.com>
2022-12-27 21:46:58 -0600

    Inserting toolbelt button sprites

---
+++ Grim_Constructor_P2_Files/Assets/Scriptable Objects/Tool.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New tool")]
+public class Tool : ScriptableObject
+{
+    public new string name;
+    public int cost;
+    public int amount;
+}
